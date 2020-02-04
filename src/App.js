5a0cb264249d6d258ead71f523252bfc3c126d9e
Mr. Jack <maesongamer@gmail.com>
2020-02-03 21:56:35 -0500

    hola

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+const people = ['Rowe', 'Prevost', 'Gare', 'hola', 'estoy', 'en', 'una', 'lista'];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    Object.keys(people).map((person, i )=> 
+    // expression goes here:
+  <li key={'person_'+i}>{person}</li>
+  
+  )
+  )
 }
 
 export default App;
